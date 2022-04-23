4169f43a324b10e0224282567acb1d55b0798618
esempe <steam.dota.return@gmail.com>
2022-04-23 21:53:38 +0300

    now divided all data in dedicated file (outside form UI), connected data with ui via props, added friendlist in SideNav block

--- src/compoNents/SideNav/SideNav.jsx
+++ src/compoNents/SideNav/SideNav.jsx
@@ -1,32 +1,36 @@
 import React from 'react'
 import style from './SideNav.module.css'
 import {
-    Link,NavLink
+    Link, NavLink
 } from "react-router-dom";
+import SideNavFriendsList from "./SideNavFreindsList/SideNavFriendsList";
 
-const SideNav = () => {
 
-    let LinkActive = ({isActive})=> {
+const SideNav = (props) => {
+    debugger;
+    let LinkActive = ({isActive}) => {
         return isActive ? style.active : style.text;
     }
 
     return (
-        <div  className={style.SideNav}>
+        <div className={style.SideNav}>
             <div className={style.item}>
-                <NavLink to='/profile' className={LinkActive} >Profiles</NavLink>
-            </div>
-            <div className={style.item}>
-                <NavLink to='dialogs/' className={LinkActive} >Messeges</NavLink>
+                <NavLink to='/profile' className={LinkActive}>Profiles</NavLink>
             </div>
             <div className={style.item}>
-                <NavLink to='/feed' className={LinkActive} >News</NavLink>
+                <NavLink to='dialogs/' className={LinkActive}>Messeges</NavLink>
             </div>
             <div className={style.item}>
-                <NavLink to='/music' className={LinkActive} >Music</NavLink>
+                <NavLink to='/feed' className={LinkActive}>News</NavLink>
             </div>
-            <div className={style.item} >
-                <NavLink to='/settings' className={LinkActive} >Settings</NavLink>
+            <div className={style.item}>
+                <NavLink to='/music' className={LinkActive}>Music</NavLink>
             </div>
+            <div className={style.item + ' ' + style.itemset }>
+                <NavLink to='/settings' className={LinkActive}>Settings</NavLink>
+            </div>
+            <SideNavFriendsList state={props.state}/>
+
         </div>
     )
 }
---
+++ src/redux/state.js
@@ -0,0 +1,47 @@
+let state = {
+    dialogs: {
+        messageData: [  //входные данные
+            {id: 1, message: 'Hy h1w are you??'},
+            {id: 2, message: 'bebra karpacho!'},
+            {id: 3, message: 'really?? you are babrj'},
+            {id: 4, message: 'noo way'},
+            {id: 5, message: 'magick is dred'},
+            {id: 6, message: 'falco and gabby'},
+            {id: 7, message: 'ramires little dirty'},
+            {id: 8, message: 'my first message!!!'},
+        ],
+        conversationsData: [ //входные данные
+            {id: 1, name: 'Rocky'},
+            {id: 2, name: 'Anrew'},
+            {id: 3, name: 'Anorick'},
+            {id: 4, name: 'Anatoly'},
+            {id: 5, name: 'Rocky'},
+            {id: 6, name: 'Rocky'},
+            {id: 7, name: 'Ramen'},
+            {id: 8, name: 'Denzlo'},
+        ]
+    },
+
+    userProfile: {
+        postsData: [
+            {id: 1, message: 'My new post', likeCount: '6'},
+            {id: 2, message: 'I love to smell bebra', likeCount: '5'},
+            {id: 3, message: '1 like and i will....', likeCount: '554'},
+            {id: 4, message: 'aye salam basota', likeCount: '12'},
+            {id: 5, message: 'magick is dred', likeCount: '1488'},
+            {id: 6, message: 'falco and gabby', likeCount: '1327'},
+            {id: 7, message: 'ramires little dirty', likeCount: '332'},
+            {id: 8, message: 'my first post!!!', likeCount: '21'},
+        ]
+    },
+    sideNav: {
+        friendsList: [
+            {id: 1, friendsName: 'Rina', friendsAva: 'ʕ•́ᴥ•̀ʔ'},
+            {id: 2, friendsName: 'DedoDed', friendsAva: 'ヽ(°□° )ノ'},
+            {id: 3, friendsName: 'Ser', friendsAva: '(•ω•)'}
+
+        ]
+    }
+
+}
+export default state;
